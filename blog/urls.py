e3a636047e875354e6e98792cc0cd9667200f531
yash <ymehta777@gmail.com>
2021-02-06 09:40:03 +0530

    Add CRUD functionality

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('', views.PostListView.as_view(), name='home'),
     path('post/<int:pk>', views.PostDetailView.as_view(), name='detail'),
     path('post/new/', views.PostCreateView.as_view(), name='create'),
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='update'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='delete'),
     path('about/', views.about, name='about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Post
 from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 # Create your views here.
 
@@ -22,13 +23,39 @@ class PostDetailView(generic.DetailView):
     template_name = 'blog/detail.html'
 
 
-class PostCreateView(generic.CreateView):
+class PostCreateView(LoginRequiredMixin, generic.CreateView):
     model = Post
     fields = ['title', 'content']
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, generic.CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, generic.DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 
 def about(request):
